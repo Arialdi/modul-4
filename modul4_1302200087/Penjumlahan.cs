25b330112a87745c7a4b2f3237e35c2a098b5707
Arialdi <arialdirahmadiputra@student.telkomuniversity.ac.id>
2022-03-19 16:16:55 +0700

    JumlahTigaAngka

---
+++ modul4_1302200087/Penjumlahan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302200087
+{
+    internal class Penjumlahan<T>
+    {
+        T data;
+        public static int JumlahTigaAngka(T an1, T an2, T an3)
+        {
+            int AN1 = Convert.ToInt32(an1);
+            int AN2 = Convert.ToInt32(an2);
+            int AN3 = Convert.ToInt32(an3);
+
+            int jumlah = AN1 + AN2 + AN3;
+
+            return jumlah;
+        }
+    }
+}
---
+++ modul4_1302200087/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul4_1302200087
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jumlahnya dari 3 angka:" + Penjumlahan<int>.JumlahTigaAngka(13, 02, 20));
+        }
+    }
+}
