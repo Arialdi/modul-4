8c8e904fb9420256e45e8d5eaed8e3e530d22bab
Arialdi <arialdirahmadiputra@student.telkomuniversity.ac.id>
2022-03-19 17:01:07 +0700

    menambahkan class SimpleDataBase

---
+++ modul4_1302200087/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302200087
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<int> db = new SimpleDataBase<int>();
+            db.AddNewData(13);
+            db.AddNewData(02);
+            db.AddNewData(20);
+            db.PrintAllData();
+        }
+    }
+}
